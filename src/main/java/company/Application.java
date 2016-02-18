0b8f8f7321f64af183760308a21362467fff4cf2
Kerwyn Rojas <kerwyn.rg@gmail.com>
2016-02-17 18:29:51 -0700

    Initialized model and repository

--- src/main/java/company/Application.java
+++ src/main/java/company/Application.java
@@ -1,7 +1,15 @@
 package company;
 
+import java.util.Arrays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Started");
+        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+        System.out.println("App Started!");
     }
 }
