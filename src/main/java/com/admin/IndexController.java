ce2d15fd79a01d3c301c247dd9c9d42996f51a34
Kerwyn Rojas <kerwyn.rg@gmail.com>
2016-02-27 18:27:15 -0700

    Added IndexController

--- src/main/java/com/admin/IndexController.java
+++ src/main/java/com/admin/IndexController.java
@@ -2,11 +2,18 @@ package com.admin;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 public class IndexController extends WebMvcConfigurerAdapter {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("*")
+            .addResourceLocations("/static/");
+    }
+
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("forward:dist/index.html");
